5086e71338b48135b8b5ccedccb0f5af49da1ede
Firdausi Baiq Ardana <firdaardana@student.ub.ac.id>
2023-09-24 00:04:03 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="assets/css/styles.css">
+
+    <title>Berbinar Psikotest</title>
+</head>
+
+<body>
+    <section class="intro_container">
+            <div class="flex flex-col items-center">
+                <h2 class="intro_title md:w-full w-[90%] text-center font-semibold text-6xl mb-8 leading-tight ">Free Tes <br> Psikotes Berbinar</h2>
+                <p class="intro_description md:w-[70%] w-[85%] text-justify">Tes ini merupakan model dari tes lima dimensi kepribadian yang dapat mengungkapkan potensi karir yang sesuai dengan kepribadian Anda. Tes Lima Dimensi Kepribadian secara luas dianggap sebagai cara paling kuat untuk menggambarkan perbedaan kepribadian yang Anda miliki. Ini adalah dasar penelitian kepribadian paling modern. <br><br> Tes Lima Dimensi Kepribadian adalah salah satu alat tes untuk mengungkap kepribadian berdasarkan teori Big Five Personality yang dikemukakan oleh seorang psikolog terkenal, yaitu Lewis Goldberg. Dalam teori sifat kepribadian "The Big Five Personality Traits Model" tersebut terdiri dari lima dimensi kunci diantaranya seperti, Openness (O), Conscientiousness (C), Extraversion (E), Agreeableness (A) dan Neuroticism (N).</p>    
+                <div class="flex gap-4 mt-8">
+                    <a href="#" class="button_white px-14 py-3 font-semibold">Mulai Tes</a>
+                    <a href="#" class="button_bg px-8 py-3 font-semibold">Kembali ke Beranda</a>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
